76c2dd39870983d8c173bb5b712036621f34c528
Edwin Cardenas <ecardenas@lwolf.com>
2026-04-05 09:01:34 -0500

    feat: zstack

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,27 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            HStack(spacing: 20) {
-                Text("1")
-                Text("2")
-                Text("3")
-            }
+        ZStack {
+            LinearGradient(
+                colors: [.pink, .purple],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
 
-            HStack(spacing: 20) {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
+            VStack(spacing: 10) {
+                HStack(spacing: 20) {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
 
-            HStack(spacing: 20) {
-                Text("7")
-                Text("8")
-                Text("9")
+                HStack(spacing: 20) {
+                    Text("4")
+                    Text("5")
+                    Text("6")
+                }
+
+                HStack(spacing: 20) {
+                    Text("7")
+                    Text("8")
+                    Text("9")
+                }
+                .fontWeight(.light)
             }
-            .fontWeight(.light)
+            .foregroundStyle(.white)
+            .font(.system(size: 70, weight: .ultraLight))
         }
-        .font(.system(size: 70, weight: .ultraLight))
     }
 }
 
